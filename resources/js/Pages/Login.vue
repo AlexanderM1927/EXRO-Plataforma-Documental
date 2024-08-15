f86652f239b721360f33fc293672488b66aaaf03
John A. Martinez <52513621+AlexanderM1927@users.noreply.github.com>
2024-08-15 17:04:28 -0500

    Created loading component

--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -2,23 +2,28 @@
 import LoginLayout from '../Layouts/LoginLayout.vue'
 import axios from 'axios';
 import { ref, inject } from 'vue'
+import Loading from '../components/Loading.vue'
 const swal = inject('$swal')
 
 const email = ref('')
 const password = ref('')
+const isLoading = ref(false)
 
 
 const login = async (e) => {
     e.preventDefault()
     try {
+        isLoading.value = true
         const request = await axios.post('/auth', {
             email: email.value,
             password: password.value
         })
+        isLoading.value = false
         if (request.status === 200) {
             location.href = request.data.redirect
         }
     } catch (error) {
+        isLoading.value = false
         swal.fire({
             title: "Oooops!",
             text: error.response.data.message,
@@ -54,4 +59,5 @@ const login = async (e) => {
             <button type="submit" class="btn btn-success">Login</button>
         </form>
     </LoginLayout>
+    <Loading :isActive="isLoading"></Loading>
 </template>
---
+++ resources/js/components/Loading.vue
@@ -0,0 +1,43 @@
+<template>
+    <div :class="`loading ${isActive ? 'loading-active' : ''}`">
+        <div class="loading-content d-flex justify-content-center align-items-center">
+            <div class="spinner-border" role="status">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(
+    {
+        isActive: {
+            type: Boolean,
+            required: true
+        }
+    }
+)
+</script>
+
+<style>
+.loading {
+    position: fixed; /* Sit on top of the page content */
+    display: none; /* Hidden by default */
+    width: 100%; /* Full width (cover the whole page) */
+    height: 100%; /* Full height (cover the whole page) */
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0,0,0,0.5); /* Black background with opacity */
+    z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
+    cursor: pointer; /* Add a pointer on hover */
+}
+
+.loading-content {
+    height: 100%;
+}
+
+.loading-active {
+    display: block;
+}
+</style>
